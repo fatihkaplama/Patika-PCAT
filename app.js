d6289afc6bbfa79d5a1ac8e63c252c21d1319631
Fatih Kaplama <fatihkaplama48@gmail.com>
2022-02-14 21:03:51 +0300

    Finished

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
+const ejs = require("ejs");
+const photoController = require("./controllers/photoController");
+const pageController = require("./controllers/pageController");
+const app = express();
+
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
+// ROUTES
+app.get("/", photoController.getAllPhotos);
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
+
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
+app.get("/photos/edit/:id", pageController.getEditPage);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
